aa011dd7ca65bede2be8fd38d6c62a25817c2c91
sazumy <azumisaito@gmail.com>
2024-01-27 17:05:50 +0900

    ざっくりメニューの開閉

--- script/index.js
+++ script/index.js
@@ -10,4 +10,14 @@ window.onload = function() {
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
   
+  // gnavの開閉
+  const gnavButton = document.getElementById('js-gnav-btn');
+  const gnavSp = document.getElementById('js-gnav-sp');
+  const openNav = ()=> {
+    gnavSp.classList.add('show');
+  }
+
+  gnavButton.addEventListener('click', openNav);
+
+  
 };
