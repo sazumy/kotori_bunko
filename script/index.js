59b36d6520ee4981960c43ed4340cbfca9209f1d
sazumy <azumisaito@gmail.com>
2024-01-28 11:57:20 +0900

    コンテンツの表示/非表示ボタンをざっくり作る

--- script/index.js
+++ script/index.js
@@ -24,4 +24,23 @@ window.onload = function() {
   gnavButton.addEventListener('click', openNav);
   gnavCloseBtn.addEventListener('click', closeNav);
   
+  // もっと見るボタンの操作
+  const hiddenContent = document.getElementById('js-hidden-content');
+  const viewMoreBtn = document.getElementById('js-view-more-btn');
+  const viewLessBtn = document.getElementById('js-view-less-btn');
+
+  const viewMore = () => {
+    hiddenContent.classList.add('show');
+    viewMoreBtn.classList.remove('show');
+    viewLessBtn.classList.add('show');
+  }
+
+  const viewLess = () => {
+    hiddenContent.classList.remove('show');
+    viewMoreBtn.classList.add('show');
+    viewLessBtn.classList.remove('show');
+  }
+
+  viewMoreBtn.addEventListener('click', viewMore);
+  viewLessBtn.addEventListener('click', viewLess);
 };
