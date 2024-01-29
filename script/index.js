93c1443435be64faef19b0a2727fbdf55abf7436
sazumy <azumisaito@gmail.com>
2024-01-29 13:39:37 +0900

    TODOメッセージ追加

--- script/index.js
+++ script/index.js
@@ -46,6 +46,7 @@ window.onload = function() {
 
   // Instagramデータ取得
   const data_url = new XMLHttpRequest();
+  // TODO: 環境変数などで管理しないと、Facebookによってトークンは無効化されてしまう。
   const token = 'IGQWROaDJTNVVTVXFGcmd5YXM2YXpWa1N4QUpfaFdvOW1PTkVvTE9RUFlQb2NZAZAUUtLTBCbGFERUc3RGJ6UmVudUFZAbXA1NlZAFT1hDQ0lEaFRmbEtXV09QeFp2dVlvMlYwVFVSdlJNTVk1STAtUG9MSmtWY2hlRmUwSnh6R3MwdWN6ZAwZDZD';
   data_url.open('GET', `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${token}`);
   data_url.send();
