28d9ba71bf9ba1ba0d2f6bc0e522cc02262e6a69
sazumy <azumisaito@gmail.com>
2024-02-13 14:14:36 +0900

    jQueryで実装

--- js/modules.js
+++ js/modules.js
@@ -1,7 +1,6 @@
 import { runAdobeFont } from "./components/runAdobeFont.js";
 import { openAndCloseGnav } from "./components/openAndCloseGnav.js";
 import { viewMoreAndLess } from "./components/viewMoreAndLess.js";
-import { showIgMedia } from "./components/showIgMedia.js";
 
 runAdobeFont(document);
 openAndCloseGnav();
@@ -11,7 +10,36 @@ viewMoreAndLess();
 
 // jQueryのコード仮置き
 $(function(){
-    const $igField= $("#js-instagram-field");
-    const htmlText = `<p>テスト</p>`
-    $igField.append(htmlText);
+    // DEV環境の時はここにtokenを入れる（そのうち.envにする）
+    showIgMedia(token)
 });
+
+const showIgMedia = (igToken) => {
+    // Instagramデータ取得
+    const data_url = new XMLHttpRequest();
+    const igRequestUrl = `https://graph.instagram.com/me/media?
+                          fields=id,media_type,media_url,username,timestamp
+                          &access_token=${igToken}`;
+
+    data_url.open('GET', igRequestUrl);
+    data_url.send();
+                          
+    const $igField= $("#js-instagram-field");
+
+    data_url.onreadystatechange = () => {
+        if(data_url.readyState === 4 && data_url.status === 200) {
+            const responseJson = JSON.parse(data_url.responseText);
+            const igImages = responseJson.data;
+            // Instagram データ表示
+            igImages.map((img) => {
+                let html = `<dev>
+                                <img src="${img.media_url}" />
+                            </dev>`
+
+            $igField.append(html);
+        })
+        
+      }
+    }
+  }
+  
